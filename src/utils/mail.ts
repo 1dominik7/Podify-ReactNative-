957e4b7eaf0d6ab1fb93abc5a22ddf60dff32c8a
1dominik7 <dominik-wicek@o2.pl>
2024-02-24 11:20:09 +0100

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch server
     Your branch is up to date with 'origin/server'.
    
     Changes to be committed:
    	modified:   src/utils/mail.ts
    	modified:   src/utils/variables.ts

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,25 +1,13 @@
-import nodemailer from "nodemailer";
 import path from "path";
-import EmailVerificationToken from "#/models/emailVerificationToken";
 import {
-  MAILTRAP_PASS,
-  MAILTRAP_USER,
-  SIGN_IN_URL,
+  AUTH_EMAIL,
+  MAILTRAP_TOKEN,
   VERIFICATION_EMAIL,
 } from "#/utils/variables";
-import { generateTemplate } from "#/mail/template";
+import { MailtrapClient } from "mailtrap";
+import fs from "fs";
 
-const generateMailTransporter = () => {
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASS,
-    },
-  });
-  return transport;
-};
+const ENDPOINT = "https://send.api.mailtrap.io/";
 
 interface Profile {
   name: string;
@@ -28,36 +16,68 @@ interface Profile {
 }
 
 export const sendVerificationMail = async (token: string, profile: Profile) => {
-  const transport = generateMailTransporter();
+  const { name, email } = profile;
+  
+  const client = new MailtrapClient({
+    endpoint: ENDPOINT,
+    token: MAILTRAP_TOKEN,
+  });
 
-  const { name, email, userId } = profile;
+  const sender = {
+    email: VERIFICATION_EMAIL,
+    name: "Verification Podify",
+  };
+  const recipients = [
+    {
+      email,
+    },
+  ];
 
-  const welcomeMessgae = `Hi ${name}, welcome to Podyfi! There are so much thing that we do for verified users. Use the given OTP to verify your email.`;
+  const welcomeImage = fs.readFileSync(
+    path.join(__dirname, "../mail/welcome.png")
+  );
 
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "Welcome message",
-    html: generateTemplate({
-      title: "Welcome to Podify",
-      message: welcomeMessgae,
-      logo: "cid:logo",
-      banner: "cid:welcome",
-      link: "#",
-      btnTitle: token,
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/images/logo.png"),
-        cid: "logo",
-      },
-      {
-        filename: "welcome.png",
-        path: path.join(__dirname, "../mail/images/welcome.png"),
-        cid: "welcome",
-      },
-    ],
+  const logoImage = fs.readFileSync(path.join(__dirname, "../mail/logo.png"));
+
+  // client
+  //   .send({
+  //     from: sender,
+  //     to: recipients,
+  //     subject: "Verification Email",
+  //     html: generateTemplate({
+  //       title: "Welcome to Podify",
+  //       message: welcomeMessgae,
+  //       logo: "cid:logo",
+  //       banner: "cid:welcome",
+  //       link: "#",
+  //       btnTitle: token,
+  //     }),
+  //     category: "Verification Mail",
+  //     attachments:[
+  //       {
+  //         filename: "welcome.png",
+  //         content_id: "welcome",
+  //         disposition: "inline",
+  //         content: welcomeImage,
+  //         type: 'image/png'
+  //       },
+  //       {
+  //         filename: "logo.png",
+  //         content_id: "logo",
+  //         disposition: "inline",
+  //         content: logoImage,
+  //         type: 'image/png'
+  //       }
+  //     ]
+  //   })
+  client.send({
+    from: sender,
+    to: recipients,
+    template_uuid: "5910b4e3-2f00-4216-a22c-b7deaa64fce8",
+    template_variables: {
+      user_name: name,
+      token,
+    },
   });
 };
 
@@ -67,69 +87,109 @@ interface Options {
 }
 
 export const sendForgetPasswordLink = async (options: Options) => {
-  const transport = generateMailTransporter();
+  // const transport = generateMailTransporter();
 
   const { email, link } = options;
 
-  const message = `We just reveived a request that you forgot your password. No problem you can use the link below and create brand new password.`;
-
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "Reset Password Link",
-    html: generateTemplate({
-      title: "Forget Password",
-      message,
-      logo: "cid:logo",
-      banner: "cid:forget_password",
-      link,
-      btnTitle: "Reset Password",
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
-        cid: "logo",
-      },
-      {
-        filename: "forget_password.png",
-        path: path.join(__dirname, "../mail/forget_password.png"),
-        cid: "forget_password",
-      },
-    ],
+  const client = new MailtrapClient({
+    endpoint: ENDPOINT,
+    token: MAILTRAP_TOKEN,
   });
+
+  const sender = {
+    email: AUTH_EMAIL,
+    name: "Password Reset",
+  };
+
+  client.send({
+    from: sender,
+    to: [{ email }],
+    template_uuid: "95a9ef6c-8d30-42b8-aeae-ef34e9286ded",
+    template_variables: {
+      user_email: email,
+      pass_reset_link: link,
+    },
+  });
+
+  // const message = `We just reveived a request that you forgot your password. No problem you can use the link below and create brand new password.`;
+
+  // transport.sendMail({
+  //   to: email,
+  //   from: VERIFICATION_EMAIL,
+  //   subject: "Reset Password Link",
+  //   html: generateTemplate({
+  //     title: "Forget Password",
+  //     message,
+  //     logo: "cid:logo",
+  //     banner: "cid:forget_password",
+  //     link,
+  //     btnTitle: "Reset Password",
+  //   }),
+  //   attachments: [
+  //     {
+  //       filename: "logo.png",
+  //       path: path.join(__dirname, "../mail/logo.png"),
+  //       cid: "logo",
+  //     },
+  //     {
+  //       filename: "forget_password.png",
+  //       path: path.join(__dirname, "../mail/forget_password.png"),
+  //       cid: "forget_password",
+  //     },
+  //   ],
+  // });
 };
 export const sendPassResetSuccessEmail = async (
   name: string,
   email: string
 ) => {
-  const transport = generateMailTransporter();
-
-  const message = `Dear ${name} we just updated your new password. You can now sign in with your new password.`;
-
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
-    subject: "Password Reset Successfully",
-    html: generateTemplate({
-      title: "Password Reset Successfully",
-      message,
-      logo: "cid:logo",
-      banner: "cid:forget_password",
-      link: SIGN_IN_URL,
-      btnTitle: "Log in",
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
-        cid: "logo",
-      },
-      {
-        filename: "forget_password.png",
-        path: path.join(__dirname, "../mail/forget_password.png"),
-        cid: "forget_password",
-      },
-    ],
+  const client = new MailtrapClient({
+    endpoint: ENDPOINT,
+    token: MAILTRAP_TOKEN,
   });
+
+  const sender = {
+    email: AUTH_EMAIL,
+    name: "Password Reset",
+  };
+
+  client.send({
+    from: sender,
+    to: [{ email }],
+    template_uuid: "67dbf68c-40da-4184-beb7-e372ed11cffb",
+    template_variables: {
+      user_name: name,
+      user_email: email,
+    },
+  });
+
+  // const transport = generateMailTransporter();
+
+  // const message = `Dear ${name} we just updated your new password. You can now sign in with your new password.`;
+
+  // transport.sendMail({
+  //   to: email,
+  //   from: VERIFICATION_EMAIL,
+  //   subject: "Password Reset Successfully",
+  //   html: generateTemplate({
+  //     title: "Password Reset Successfully",
+  //     message,
+  //     logo: "cid:logo",
+  //     banner: "cid:forget_password",
+  //     link: SIGN_IN_URL,
+  //     btnTitle: "Log in",
+  //   }),
+  //   attachments: [
+  //     {
+  //       filename: "logo.png",
+  //       path: path.join(__dirname, "../mail/logo.png"),
+  //       cid: "logo",
+  //     },
+  //     {
+  //       filename: "forget_password.png",
+  //       path: path.join(__dirname, "../mail/forget_password.png"),
+  //       cid: "forget_password",
+  //     },
+  //   ],
+  // });
 };

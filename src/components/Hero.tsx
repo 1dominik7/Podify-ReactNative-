27519cc51fe78df398b91581fe6f2715d97e4f49
1dominik7 <dominik-wicek@o2.pl>
2024-04-02 19:57:55 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
    	modified:   src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,12 @@ const Hero = () => {
             </h1>
           </div>
           <p className="text-gray-900">
-          Embark on an auditory adventure with Podify - your passport to an eclectic mix of genres, from soothing melodies to pulsating beats. Explore new sounds, discover hidden gems, and let the music take you on a captivating voyage through rhythm and melody. Whether you're seeking relaxation or stimulation, Podify is your ultimate destination for immersive musical exploration
+            Embark on an auditory adventure with Podify - your passport to an
+            eclectic mix of genres, from soothing melodies to pulsating beats.
+            Explore new sounds, discover hidden gems, and let the music take you
+            on a captivating voyage through rhythm and melody. Whether you're
+            seeking relaxation or stimulation, Podify is your ultimate
+            destination for immersive musical exploration
           </p>
 
           <ToolTip tooltip="Full Stack App">
@@ -25,10 +30,10 @@ const Hero = () => {
             <a
               rel="noreferrer noopener"
               target="_blank"
-              href="https://www.udemy.com/user/niraj-dhungana-92/"
+              href="https://github.com/1dominik7/Podify-ReactNative-"
               className="bg-[#7e89fd] hover:bg-[#6c78fe] text-white font-bold px-10 py-2 rounded-md inline-block cursor-pointer"
             >
-              Take It
+              Check It
             </a>
           </div>
         </div>
